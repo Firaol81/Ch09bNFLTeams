7ae524ae52426b09369ac5f7e5b71022cc864fa0
firaol81 <firaol.baneta@southeasttech.edu>
2024-02-08 11:27:27 -0600

    Add project files.

---
+++ NFLTeams/Controllers/NameController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NFLTeams.Models;
+
+namespace NFLTeams.Controllers
+{
+    public class NameController : Controller
+    {
+        private readonly NFLSession _session;
+
+        public NameController(NFLSession session)
+        {
+            _session = session;
+        }
+
+        public IActionResult Index()
+        {
+            string userName = _session.GetUserName();
+
+            var viewModel = new TeamListViewModel
+            {
+                UserName = userName,
+            };
+            return View(viewModel);
+        }
+
+
+        [HttpPost]
+        public RedirectToActionResult Change(TeamListViewModel model)
+        {
+            var session = new NFLSession(HttpContext.Session);
+            session.SetUserName(model.UserName);
+            return RedirectToAction("Index", "Home", new
+            {
+                ActiveConf = session.GetActiveConf(),
+                ActiveDiv = session.GetActiveDiv()
+            });
+        }
+
+
+        // Add other action methods as needed
+    }
+}
---
+++ NFLTeams/Models/NFLSession.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Http;
+
+namespace NFLTeams.Models
+{
+    public class NFLSession
+    {
+        private const string TeamsKey = "myteams";
+        private const string CountKey = "teamcount";
+        private const string ConfKey = "conf";
+        private const string DivKey = "div";
+        //Here is the UserName get and set. method
+        private const string UserNameKey = "username";
+
+        private ISession session { get; set; }
+        public NFLSession(ISession session) {
+            this.session = session;
+        }
+
+        // Here is the cod e methods for getting and setting the user's name
+        public void SetUserName(string userName) =>
+            session.SetString(UserNameKey, userName);
+
+        public string GetUserName() => session.GetString(UserNameKey);
+
+        public void SetMyTeams(List<Team> teams) {
+            session.SetObject(TeamsKey, teams);
+            session.SetInt32(CountKey, teams.Count);
+        }
+        public List<Team> GetMyTeams() =>
+            session.GetObject<List<Team>>(TeamsKey) ?? new List<Team>();
+        public int? GetMyTeamCount() => session.GetInt32(CountKey);
+
+        public void SetActiveConf(string conference) =>
+            session.SetString(ConfKey, conference);
+        public string GetActiveConf() => session.GetString(ConfKey);
+
+        public void SetActiveDiv(string division) =>
+            session.SetString(DivKey, division);
+        public string GetActiveDiv() => session.GetString(DivKey);
+
+        public void RemoveMyTeams() {
+            session.Remove(TeamsKey);
+            session.Remove(CountKey);
+        }
+    }
+}
